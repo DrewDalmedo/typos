4f9ddf2e0d7fd163987258f24825f0a2e6cc4bc8
DrewDalmedo <andrewdalmedo@gmail.com>
2021-02-12 13:22:35 -0500

    Remove boilerplate and check if Header is rendered

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('header should exist', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+  const headerElement = screen.getByText("typos");
+  expect(headerElement).toBeInTheDocument();
+})
