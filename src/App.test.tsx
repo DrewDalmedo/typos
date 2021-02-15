033a6712e17a5a22a42409d62c7a825471b78998
DrewDalmedo <andrewdalmedo@gmail.com>
2021-02-15 12:57:17 -0500

    Add test for footer component

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -6,4 +6,10 @@ test('header should exist', () => {
   render(<App />);
   const headerElement = screen.getByText("typos");
   expect(headerElement).toBeInTheDocument();
-})
+});
+
+test('footer should exist', () => {
+  render(<App />)
+  const footerElement = document.getElementsByClassName('footer');
+  expect(footerElement).toBeInTheDocument();
+});
