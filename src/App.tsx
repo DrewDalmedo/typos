1a1699f4e0484dbe81d5b52e21ba0724600e93fe
DrewDalmedo <andrewdalmedo@gmail.com>
2021-02-16 12:53:41 -0500

    Add Footer component to app

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 // import './App.css';
 
 import Header from './components/header/Header'
+import Footer from './components/footer/Footer'
 
 const App = () => {
   return (
     <div>
       <Header />
+      {/* add content component here */}
+      <Footer />
     </div>
   );
 }
