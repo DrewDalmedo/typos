e5661d7b892e855a0e9bfe7c6fad12c31be5de71
DrewDalmedo <andrewdalmedo@gmail.com>
2021-02-16 12:57:19 -0500

    Fix hrefs so they properly link to their pages

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,8 +3,8 @@ import React from 'react';
 const Footer = () => {
   return (
     <div className="footer" >
-      <a href="drewdalmedo.com" target='_blank'>Portfolio</a>
-      <a href='github.com/DrewDalmedo' target='_blank'>GitHub</a>
+      <a href="https://drewdalmedo.com" target='_blank'>Portfolio</a>
+      <a href='https://github.com/DrewDalmedo' target='_blank'>GitHub</a>
       <a href='mailto:drew@drewdalmedo.com'>Contact</a>
     </div>
   );
