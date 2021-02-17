816e9a2725efd3691f64be9cc3183c63fb3ce696
DrewDalmedo <andrewdalmedo@gmail.com>
2021-02-17 14:41:12 -0500

    Create word actions for creating words

---
+++ src/store/words/actions.ts
@@ -0,0 +1,15 @@
+// actions
+import randomWords from 'random-words';
+import { GEN_WORDS, ADD_WORDS, WordActionTypes } from './types';
+
+export function generateWords(count: number): WordActionTypes {
+  const genWords = randomWords(count)
+  return addWords(genWords)
+}
+
+export function addWords(words: string | string[]): WordActionTypes {
+  return {
+    type: ADD_WORDS,
+    payload: words
+  }
+}
