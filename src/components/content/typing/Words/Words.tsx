314b4b1594d5c15ab35439da715e4efa3eed473d
DrewDalmedo <andrewdalmedo@gmail.com>
2021-02-17 14:42:26 -0500

    Implement Words component

--- src/components/content/typing/Words/Words.tsx
+++ src/components/content/typing/Words/Words.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useSelector, useDispatch, connect } from 'react-redux'
+import { WordsState } from "../../../../store/words/reducer";
 
 const Words = () => {
+  const words: string[] = useSelector( (state: WordsState ) => [...state.words] )
   return (
     <div>
-
+      {words.join(" ")}
     </div>
   );
 };
