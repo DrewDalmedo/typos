060b8e00ad16752b52668ac6cc20c33fba50d26f
DrewDalmedo <andrewdalmedo@gmail.com>
2021-02-17 14:42:56 -0500

    Add Words component to Content component

--- src/components/content/Content.tsx
+++ src/components/content/Content.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { generateWords } from '../../store/words/actions';
+import Words from './typing/Words/Words';
 
 const Content = () => {
+  const dispatch = useDispatch()
+  dispatch(generateWords(25))
   return (
     <div>
-
+      <Words />
     </div>
   );
 };
