18e4024d59c17b14f46c9836aed4843edfc0fc2b
DrewDalmedo <andrewdalmedo@gmail.com>
2021-02-17 14:41:33 -0500

    Implement the words reducer

---
+++ src/store/words/reducer.ts
@@ -0,0 +1,36 @@
+import { Reducer } from 'react';
+import { GEN_WORDS, ADD_WORDS } from './types';
+
+export interface WordsState {
+  words: string[]
+  generating: boolean
+}
+
+const initialState: WordsState = {
+  words: [],
+  generating: false
+}
+
+type Action = {type: string, payload?: string[]}
+
+export default function wordsReducer(
+  state: WordsState = initialState, 
+  action: Action
+): any {
+  switch(action.type) {
+    case GEN_WORDS:
+      return {
+        ...state,
+        words: [...state.words],
+        generating: true
+      }
+    case ADD_WORDS:
+      return {
+        ...state,
+        words: action.payload,
+        generating: false 
+      }
+    default:
+      return state
+  }
+}
