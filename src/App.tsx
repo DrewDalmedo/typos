ed28669286b8ff139be13ad3168702b362185497
DrewDalmedo <andrewdalmedo@gmail.com>
2021-02-17 14:36:06 -0500

    Implement redux and the Content component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
 import React from 'react';
 // import './App.css';
 
+// redux imports
+import { Provider } from 'react-redux';
+import { store } from './store/store'
+
+// components
 import Header from './components/header/Header'
 import Footer from './components/footer/Footer'
+import Content from './components/content/Content';
 
 const App = () => {
   return (
-    <div>
-      <Header />
-      {/* add content component here */}
-      <Footer />
-    </div>
+    <Provider store={store}>
+      <div>
+        <Header />
+        <Content />
+        <Footer />
+      </div>
+    </Provider>
   );
 }
 
