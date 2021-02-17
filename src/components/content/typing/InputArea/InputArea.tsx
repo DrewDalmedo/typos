5d9a2b0e77502e184f3e3664808ebde2407ced8f
DrewDalmedo <andrewdalmedo@gmail.com>
2021-02-17 15:06:14 -0500

    Implement InputArea functionality

--- src/components/content/typing/InputArea/InputArea.tsx
+++ src/components/content/typing/InputArea/InputArea.tsx
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { generateWords } from '../../../../store/words/actions';
 
 const InputArea = () => {
+
+  const [userInput, setUserInput] = useState('');
+
+  const dispatch = useDispatch();
+
+  const handleInputChange = (event: any) => {
+    const currentText: string = event.target.value;
+
+    if (currentText[currentText.length - 1] === ' ') {
+      setUserInput('')
+    }
+    else {
+      setUserInput(currentText)
+    }
+  }
+
+  const handleRedoClick = (event: any) => {
+    dispatch(generateWords(25))
+  }
+
   return (
     <div>
-
+      <input type="text" value={userInput} onChange={handleInputChange}/>
+      <button onClick={handleRedoClick}>Redo</button>
     </div>
   );
 };
