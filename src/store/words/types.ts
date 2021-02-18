7bfd310334799dc94ec313c5e79044e8d34afcac
DrewDalmedo <andrewdalmedo@gmail.com>
2021-02-18 12:51:47 -0500

    Remove unused types

--- src/store/words/types.ts
+++ src/store/words/types.ts
@@ -1,14 +1,9 @@
 // words
-export const GEN_WORDS: string = "GEN_WORDS"
 export const ADD_WORDS: string = "ADD_WORDS"
-
-interface GenerateWordsAction {
-  type: typeof GEN_WORDS,
-}
 
 interface AddWordsAction {
   type: typeof ADD_WORDS,
   payload: string | string[]
 }
 
-export type WordActionTypes = GenerateWordsAction | AddWordsAction
+export type WordActionTypes = AddWordsAction
