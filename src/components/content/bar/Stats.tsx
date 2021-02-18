4bfe4190fddab0856aeb081b6b44d191a9d45436
DrewDalmedo <andrewdalmedo@gmail.com>
2021-02-18 12:44:55 -0500

    Make dummy WPM and Accuracy components

--- src/components/content/bar/Stats.tsx
+++ src/components/content/bar/Stats.tsx
@@ -1,11 +1,39 @@
 // for displaying wpm and accuracy
 import React from 'react';
 
-const Stats = () => {
+interface WPMProps {
+  wpm?: number
+}
+
+interface AccuracyProps {
+  acc?: number
+}
+
+interface StatsProps {
+
+}
+
+const WPM = (props: WPMProps) => {
   return (
     <>
-
+      <span>{props.wpm ? `${props.wpm}` : '--'} WPM</span>
     </>
+  );
+}
+
+const Accuracy = (props: AccuracyProps) => {
+  return (
+    <>
+      <span>{props.acc ? `${props.acc}` : '--'}% Accuracy</span>
+    </>
+  );
+}
+
+const Stats = (props: StatsProps) => {
+  return (
+    <div>
+      <WPM wpm={1} /> / <Accuracy acc={100}/>
+    </div>
   );
 }
 
