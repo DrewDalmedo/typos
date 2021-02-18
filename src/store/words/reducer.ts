66da00e0acce599e09d05eda78f38aba80399f93
DrewDalmedo <andrewdalmedo@gmail.com>
2021-02-18 12:52:31 -0500

    Remove unused GEN_WORDS handling

--- src/store/words/reducer.ts
+++ src/store/words/reducer.ts
@@ -1,33 +1,22 @@
-import { GEN_WORDS, ADD_WORDS } from './types';
+import { ADD_WORDS, WordActionTypes } from './types';
 
 export interface WordsState {
   words: string[]
-  generating: boolean
 }
 
 const initialState: WordsState = {
   words: [],
-  generating: false
 }
-
-type Action = {type: string, payload?: string[]}
 
 export default function wordsReducer(
   state: WordsState = initialState, 
-  action: Action
+  action: WordActionTypes 
 ): any {
   switch(action.type) {
-    case GEN_WORDS:
-      return {
-        ...state,
-        words: [...state.words],
-        generating: true
-      }
     case ADD_WORDS:
       return {
         ...state,
         words: action.payload,
-        generating: false 
       }
     default:
       return state
