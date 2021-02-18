143efaaad958c8fea0e320bb663d29ad8fc5fc82
DrewDalmedo <andrewdalmedo@gmail.com>
2021-02-17 21:36:23 -0500

    Add WordCount and Stats components

--- src/components/content/bar/Bar.tsx
+++ src/components/content/bar/Bar.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import { useDispatch } from "react-redux";
+import { generateWords } from '../../../store/words/actions';
+
+
+import Stats from './Stats';
+import WordCount from './WordCount';
 
 const Bar = () => {
+  const dispatch = useDispatch()
+
+  const changeWordCount = (count:number) => {
+    dispatch(generateWords(count));
+  }
+
   return (
     <div>
-      
+      <WordCount changeWordCount={changeWordCount} />
+      <Stats />
     </div>
   );
 };
