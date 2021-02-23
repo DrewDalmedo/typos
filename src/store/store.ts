38c506f23879b73720effd495f4e4182fe7c4459
DrewDalmedo <andrewdalmedo@gmail.com>
2021-02-23 15:03:47 -0500

    Connect the store to redux devtools

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,5 +1,12 @@
 import { createStore } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import wordsReducer from './words/reducer';
 
-export const store = createStore(wordsReducer);
+export const store = createStore(
+  wordsReducer, 
+  // connect to redux devtools
+  composeWithDevTools(
+    // middleware and options go here
+  )
+);
