8a62e08707955a344a45cb99c903a914d09a1b51
DrewDalmedo <andrewdalmedo@gmail.com>
2021-02-23 15:54:04 -0500

    Generate 25 words by default

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,13 @@ import React from 'react';
 import Header from './components/header/Header'
 import Footer from './components/footer/Footer'
 import Content from './components/content/Content';
+import { useDispatch } from 'react-redux';
+import { generateWords } from './store/words/actions';
 
 const App = () => {
+  const dispatch = useDispatch();
+  dispatch(generateWords(25))
+
   return (
       <div>
         <Header />
