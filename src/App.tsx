baca0cbfe6763b303780b95e012bef7c4330fdd5
DrewDalmedo <andrewdalmedo@gmail.com>
2021-02-23 15:44:00 -0500

    Move provider from App to index

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
 // import './App.css';
 
-// redux imports
-import { Provider } from 'react-redux';
-import { store } from './store/store'
-
 // components
 import Header from './components/header/Header'
 import Footer from './components/footer/Footer'
@@ -12,13 +8,11 @@ import Content from './components/content/Content';
 
 const App = () => {
   return (
-    <Provider store={store}>
       <div>
         <Header />
         <Content />
         <Footer />
       </div>
-    </Provider>
   );
 }
 
