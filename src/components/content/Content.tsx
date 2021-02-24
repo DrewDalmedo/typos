c2895befbd84740797e00a1772fc789223eda3a6
DrewDalmedo <andrewdalmedo@gmail.com>
2021-02-24 13:54:43 -0500

    Calculate accuracy on each word submission

--- src/components/content/Content.tsx
+++ src/components/content/Content.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { incrementCorrect, incrementCurrentWord } from '../../store/words/actions';
+import { calculateAccuracy, incrementCorrect, incrementCurrentWord } from '../../store/words/actions';
 import { WordsState } from '../../store/words/reducer';
 import Bar from './bar/Bar';
 import InputArea from './typing/InputArea/InputArea';
@@ -11,6 +11,7 @@ const Content = () => {
 
   const words = useSelector( (state: WordsState) => state.words)
   const currentWordIndex = useSelector( (state: WordsState) => state.currentWordIndex )
+  const correct = useSelector( (state: WordsState) => state.correct )
 
   const handleWordSubmission = (submission: string): void => {
     // check to see if the submitted word is equal to the words[currentWordIndex]
@@ -20,11 +21,17 @@ const Content = () => {
     // prevent oob error by checking if the currentWordIndex isn't greater or equal to the length of words
     if (!(currentWordIndex >= words.length)) {
       const currentWord = words[currentWordIndex]
+      // useSelector only updates on rerender, which means if we increment correct inside this function, the correct var outside of this scope won't be updated until the function is done running
+      let localCorrect = correct;
 
-      if (submission === currentWord) 
+      if (submission === currentWord) {
         dispatch(incrementCorrect()) 
+        localCorrect += 1
+      }
       
       dispatch(incrementCurrentWord()) 
+
+      dispatch(calculateAccuracy(currentWordIndex, localCorrect))
 
     }
   }
