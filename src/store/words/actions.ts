5b63184356a1583b074ab29a386ee2ba514bb7fc
DrewDalmedo <andrewdalmedo@gmail.com>
2021-02-24 13:46:51 -0500

    Implement actions to calculate WPM and accuracy

--- src/store/words/actions.ts
+++ src/store/words/actions.ts
@@ -1,6 +1,6 @@
 // actions
 import randomWords from 'random-words';
-import { ADD_WORDS, INC_CURRENT_WORD, INC_CORRECT, WordActionTypes } from './types';
+import { ADD_WORDS, INC_CURRENT_WORD, INC_CORRECT, WordActionTypes, CALC_WPM, CALC_ACC } from './types';
 
 export function generateWords(count: number): WordActionTypes {
   const genWords = randomWords(count)
@@ -25,5 +25,21 @@ export function incrementCorrect(): WordActionTypes {
   return {
     type: INC_CORRECT,
     payload: []
+  }
+}
+
+export function calculateWPM(wordCount: number, time: number): WordActionTypes {
+
+  return {
+    type: CALC_WPM,
+    payload: `${1}` // change from 1 to calculation performed before the return
+  }
+}
+
+export function calculateAccuracy(currentWordIndex: number, correct: number): WordActionTypes {
+  const accuracy = (correct / (currentWordIndex+1)) * 100;
+  return {
+    type: CALC_ACC,
+    payload: `${accuracy}` // change from 1 to calculation performed before the return
   }
 }
